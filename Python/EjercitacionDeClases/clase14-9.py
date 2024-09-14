eb36a539c7fc21ed0cfb641edc630c36d15e256a
Cristian Manuel Veliz <cristianmanuelveliz@gmail.com>
2024-09-14 11:21:48 +0200

    sumando cambios a la clase del 14-9 sobre tipos de variables en python

---
+++ Python/EjercitacionDeClases/clase14-9.py
@@ -0,0 +1,16 @@
+#Simples
+#int= números enteros sin valor decimal
+#float= números con valor decimal
+#str=cuando utilizo "" y son cadena de caracteres
+#bool = Cuando utilizo False o True
+#Nonetype = Cuando utilizo None
+
+#Estructurados
+#Listas: utilizo []
+#Dict: utilizo {}
+#Tuplas: utilizo ()
+
+# para saber el tipo: type(nombre la variable)
+
+x = None
+print(type(x))
