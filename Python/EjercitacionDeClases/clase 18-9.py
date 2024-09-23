32acaaf336d23d8744522ba8e77e4404aaa95624
Cristian Manuel Veliz <cristianmanuelveliz@gmail.com>
2024-09-23 09:56:50 +0200

    sume clase 18-9

---
+++ Python/EjercitacionDeClases/clase 18-9.py
@@ -0,0 +1,32 @@
+#Escribe un codigo para verificar si un numero es par o impar
+#numero1 = int(input("ingrese un numero "))
+#if numero1 % 2 == 0:
+#    print("El numero :",numero1," es par")
+#else :
+#    print ("El numero :",numero1," es impar")
+
+#Funciones
+#def CalcularSuma(numero1,numero2):
+#    resultado = int (numero1) - int (numero2)
+#    print (resultado)
+#    return resultado
+
+#CalcularSuma(3,4)
+#numero1 = 5
+#print (numero1)
+
+''' Bloque comentario
+n = int(input("ingrese la cantidad de veces a imprimir"))
+for i in range(n):
+    print(i+5)
+
+
+nombre = input("Ingrese su nombre ")
+print("Hola ",nombre )
+'''
+animales = ['perro','gato','oso']
+
+print(animales[0])
+animales[2] = 'elefante'
+print(animales[2])
+
